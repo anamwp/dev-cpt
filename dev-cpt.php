7b251b8365b12577e6936a210803c4fa20de4dec
anam <anam.hossain@multidots.com>
2025-05-24 16:36:39 +0600

    initial commit

---
+++ dev-cpt.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Dev Cpt
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     dev-cpt
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Dev_Cpt
+ */
+
+// Your code starts here.
