f84f58d06c1948f7e249dd25d7b9cf4fa08e4a83
anam <anam.hossain@multidots.com>
2025-05-26 12:45:29 +0600

    [new] demo and brew cpt and shortocde added

--- dev-cpt.php
+++ dev-cpt.php
@@ -1,15 +1,43 @@
 <?php
 /**
- * Plugin Name:     Dev Cpt
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin Name:     Dev CPT
+ * Plugin URI:      https://anam.rocks
+ * Description:     A plugin to demonstrate custom post types and taxonomies.
+ * Author:          Anam
+ * Author URI:      https://anam.rocks
+ * License:         GPL-2.0-or-later
+ * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:     dev-cpt
  * Domain Path:     /languages
  * Version:         0.1.0
  *
- * @package         Dev_Cpt
+ * @package         devcpt
+ * @author          Anam
  */
 
+use Anam\GutenbergStarter\Shortcode\Shortcode_Breweries;
+use Anam\GutenbergStarter\Shortcode\Shortcode_Demo;
+
 // Your code starts here.
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+/**
+ * Include the custom post type files.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'inc/cpt/brew.php';
+require_once plugin_dir_path( __FILE__ ) . 'inc/cpt/demo.php';
+require_once plugin_dir_path( __FILE__ ) . 'inc/shortcode/demo.php';
+require_once plugin_dir_path( __FILE__ ) . 'inc/shortcode/brew.php';
+/**
+ * Initialize the custom post types.
+ */
+Breweries::init();
+Shortcode_Breweries::init();
+Demo::init();
+Shortcode_Demo::init();
+/**
+ * Include the custom taxonomy files.
+ */
+//phpcs:ignore require_once plugin_dir_path( __FILE__ ) . 'inc/taxonomy/brewery-type.php';
